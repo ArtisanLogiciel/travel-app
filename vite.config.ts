dc9a502560d183aba7c3b7f7094efd0fa1eec953
Michel-Developpement <a.copiercoller@gmail.com>
2024-06-30 10:58:18 +0200

    alias and tests added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig, UserConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./setupTest.ts"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+} as UserConfig);
